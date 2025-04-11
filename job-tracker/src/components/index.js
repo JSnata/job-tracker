0fb532a42fb08ce77abfa50a7f05968afd918a79
JSNata <n.kulikouskaya@gmail.com>
2025-04-11 12:09:53 +0200

    feat: implements allJobs page

--- job-tracker/src/components/index.js
+++ job-tracker/src/components/index.js
@@ -4,5 +4,9 @@ import Navbar from "./Navbar";
 import SmallSideBar from "./SmallSideBar";
 import BigSidebar from "./BigSidebar";
 import FormSelect from "./FormSelect";
+import Job from "./Job";
+import JobsContainer from "./JobsContainer";
+import SearchContainer from "./SearchContainer";
 
-export { Logo, FormRow, Navbar, SmallSideBar, BigSidebar, FormSelect };
+
+export { Logo, FormRow, Navbar, SmallSideBar, BigSidebar, FormSelect, Job, JobsContainer, SearchContainer };
